7e53fede97756aea147d60959f0611860f41c9d7
Arturo Guevara <arturo.guevara@alumno.buap.mx>
2026-03-29 09:39:17 -0600

    feat: login funcional conectado entre Angular, Django y base de datos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AuthLoginComponent } from './auth-login/auth-login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: AuthLoginComponent }
+];
--- frontend/src/app/auth-login/auth-login.ts
+++ frontend/src/app/auth-login/auth-login.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'; 
 
 @Component({
   selector: 'app-auth-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './auth-login.html',
-  styleUrl: './auth-login.css',
+  styleUrls: ['./auth-login.css']
 })
-export class AuthLogin {}
+export class AuthLoginComponent {
+  loginForm: FormGroup;
+  http = inject(HttpClient); 
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const loginData = {
+        username: this.loginForm.value.email, 
+        password: this.loginForm.value.password
+      };
+
+      this.http.post('http://localhost:8000/api/login/', loginData).subscribe({
+        next: (response: any) => {
+          console.log('¡Login Exitoso!', response);
+          alert('¡Funciona! Tu token es: ' + response.token);
+        },
+        error: (err) => {
+          console.error('Error al iniciar sesión', err);
+          alert('Credenciales incorrectas');
+        }
+      });
+
+    }
+  }
+}
