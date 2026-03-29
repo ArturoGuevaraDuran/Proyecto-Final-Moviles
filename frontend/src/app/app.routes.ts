bf8fc8a255adc5d16408674e69b82095cfa07eda
Arturo Guevara <arturo.guevara@alumno.buap.mx>
2026-03-29 10:10:45 -0600

    feat: pantalla de registro con validaciones de matricula BUAP y redireccion

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AuthLoginComponent } from './auth-login/auth-login';
+import { RegisterComponent } from './auth-register/auth-register'; 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: AuthLoginComponent }
+  { path: 'login', component: AuthLoginComponent },
+  { path: 'register', component: RegisterComponent } 
 ];
